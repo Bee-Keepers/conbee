7ca0fe530dab6a30f759545012cb21f29c09d88c
FireCurry <firecurry@naver.com>
2023-11-22 15:12:37 +0900

    프로젝트 기본틀

---
+++ conbee/src/main/java/com/keepers/conbee/ConbeeApplication.java
@@ -0,0 +1,13 @@
+package com.keepers.conbee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConbeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConbeeApplication.class, args);
+	}
+
+}
---
+++ conbee/src/test/java/com/keepers/conbee/ConbeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keepers.conbee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConbeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
