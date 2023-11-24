96ca4be9238536982809e17face83c4a0b88a0ce
FireCurry <firecurry@naver.com>
2023-11-24 17:47:38 +0900

    23 11 24 김민석

--- POS/src/main/java/com/keepers/POS/PosApplication.java
+++ POS/src/main/java/com/keepers/POS/PosApplication.java
@@ -2,8 +2,9 @@ package com.keepers.POS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class PosApplication {
 
 	public static void main(String[] args) {
--- conbee/src/main/java/com/keepers/conbee/ConbeeApplication.java
+++ conbee/src/main/java/com/keepers/conbee/ConbeeApplication.java
@@ -2,8 +2,9 @@ package com.keepers.conbee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ConbeeApplication {
 
 	public static void main(String[] args) {
