a70fe90f2e1525ecfb9d6d4d83715a5d788cda84
FireCurry <firecurry@naver.com>
2023-11-27 17:51:09 +0900

    23 11 27 김민석

--- POS/src/main/java/com/keepers/POS/main/controller/MainController.java
+++ POS/src/main/java/com/keepers/POS/main/controller/MainController.java
@@ -1,14 +1,15 @@
 package com.keepers.POS.main.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.keepers.POS.main.model.service.MainService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
 	
-	@RequestMapping("/")
-	public String mainPage() {
-		return "index";
-	}
+	private final MainService service;
 
 }
