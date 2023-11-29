d94234189b9b6eb27813a91d88b21ea73e133b11
lani <poikl11234@naver.com>
2023-11-29 16:16:50 +0900

    231129 yerina 관리자-점포관리 프론트 완료

--- conbee/src/main/resources/static/js/admin/storeUpdate.js
+++ conbee/src/main/resources/static/js/admin/storeUpdate.js
@@ -17,11 +17,7 @@ function sample6_execDaumPostcode() {
           }
 
           // 우편번호와 주소 정보를 해당 필드에 넣는다.
-          document.getElementById('sample6_postcode').value = data.zonecode;
-          document.getElementById("sample6_address").value = addr;
-          
-          // 커서를 상세주소 필드로 이동한다.
-          document.getElementById("sample6_detailAddress").focus();
+          document.getElementById("storeAddress").value = addr;
       }
   }).open();
 }
