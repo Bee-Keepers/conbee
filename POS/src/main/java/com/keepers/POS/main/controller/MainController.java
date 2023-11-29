321b1bddf657c5f690dbf6a4ef92b510150cc6fe
FireCurry <firecurry@naver.com>
2023-11-29 17:55:02 +0900

    23 11 29 김민석 포스기 품목 검색 기능, 로그인 화면 등..

--- POS/src/main/java/com/keepers/POS/main/controller/MainController.java
+++ POS/src/main/java/com/keepers/POS/main/controller/MainController.java
@@ -7,21 +7,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.keepers.POS.main.model.dto.Goods;
+import com.keepers.POS.main.model.dto.Member;
 import com.keepers.POS.main.model.service.MainService;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
+@SessionAttributes({"logimMember"})
 public class MainController {
 	
 	private final MainService service;
 	
 	@RequestMapping("/")
-	public String mainPage() {
-		return "index";
+	public String mainPage(@SessionAttribute(value = "loginMember", required = false) Member loginMember) {
+		
+		// 로그인 된 경우
+		if(loginMember != null) {
+			return "main";
+		}
+		return "login";
+	}
+	
+	
+	
+	@RequestMapping("main")
+	public String loginPage() {
+		return "main";
 	}
 	
 	@GetMapping(value = "search", produces = "application/json")
---
+++ POS/src/main/java/com/keepers/POS/main/model/dto/Member.java
@@ -0,0 +1,28 @@
+package com.keepers.POS.main.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Member {
+	private int memberNo;
+	private String memberId;
+	private String memberPw;
+	private String memberEmail;
+	private String memberName;
+	private String memberAddress;
+	private int memberTel;
+	private String memberProfile;
+	private String memberAuthority;
+	private String memberEnrollDate;
+	private String memberDelFl;
+	
+	
+	
+	private int teamNo;
+	private int departmentNo;
+	private int gradeNo;
+}
