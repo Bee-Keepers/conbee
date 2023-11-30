4acc949ac7ba841153f24efaa026cd08f03fff69
FireCurry <firecurry@naver.com>
2023-11-30 17:51:37 +0900

    포스기 데이터 디비에 삽입중

--- POS/src/main/java/com/keepers/POS/main/controller/MainController.java
+++ POS/src/main/java/com/keepers/POS/main/controller/MainController.java
@@ -1,24 +1,30 @@
 package com.keepers.POS.main.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.keepers.POS.main.model.dto.Goods;
+import com.keepers.POS.main.model.dto.History;
 import com.keepers.POS.main.model.service.MainService;
 import com.keepers.POS.member.model.dto.Member;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /** @author 김민석
  * 
  */
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @SessionAttributes({"logimMember"})
@@ -55,8 +61,22 @@ public class MainController {
 	
 	
 	@PostMapping("insert")
-	public String insert() {
+	public String insert(@SessionAttribute Member loginMember, @RequestParam("historyDiscount") List<Integer> historyDiscount, @RequestParam("historyUnitPrice") List<Integer> historyUnitPrice,
+			@RequestParam("historyGoodsName") List<String> historyGoodsName, @RequestParam("historyAmount") List<Integer> historyAmount, @RequestParam("historyActualPrice") List<Integer> historyActualPrice, @RequestParam("goodsNo") List<Integer> goodsNo,
+			@RequestParam("historyStoreName") String historyStoreName) {
 		
+		List<History> historyList = new ArrayList<>();
+		for(int i = 0; i < historyDiscount.size(); i++) {
+			History history = new History();
+			history.setGoodsNo(goodsNo.get(i));
+			history.setHistoryDiscount(historyDiscount.get(i));
+			history.setHistoryUnitPrice(historyUnitPrice.get(i));
+			history.setHistoryGoodsName(historyGoodsName.get(i));
+			history.setHistoryAmount(historyAmount.get(i));
+			history.setHistoryActualPrice(historyActualPrice.get(i));
+			history.setHistoryStoreName(historyStoreName);
+			historyList.add(history);
+		}
 		return "redirect:/";
 	}
 
