f19e35a7386f3c4518ebb8c51cc37d5881826c2d
gamza2695 <gamza2695@gmail.com>
2023-11-30 12:45:29 +0900

    231130 yoojin-1 - 템플릿 6개 프론트 작업 - 기안문 작성 : 작성 모달 기능 구현중

--- conbee/src/main/resources/static/js/approval/writeApproval.js
+++ conbee/src/main/resources/static/js/approval/writeApproval.js
@@ -32,32 +32,38 @@ function clickchk(e){
 
 // =====================================================================
 
-/* 작성 버튼 클릭 시 페이지 요청 */
-const writeBtn = document.getElementById("writeBtn");
-const checkboxAll = document.querySelectorAll('input[type="checkbox"]');
-var checkedDoc;
+// /* 작성 버튼 클릭 시 페이지 요청 */
+// const writeBtn = document.getElementById("writeBtn");
+// const checkboxAll = document.querySelectorAll('input[type="checkbox"]');
+// var checkedDoc;
 
-writeBtn.addEventListener("click", ()=> {
+// writeBtn.addEventListener("click", ()=> {
 
-  // 선택한 문서값 찾기
-  checkboxAll.forEach(k => {
-    if(k.checked == true){
-      checkedDoc = k;
-      console.log(checkedDoc.value);
-    }
-  })
+//   // 선택한 문서값 찾기
+//   checkboxAll.forEach(k => {
+//     if(k.checked == true){
+//       checkedDoc = k;
+//       console.log(checkedDoc.value);
+//     }
+//   })
 
-  // 선택한 문서가 없는 경우 알림
-  // 디자인 추가?
-  if(checkedDoc==null){
-    alert("문서를 선택해주세요");
-  }
+//   // 선택한 문서가 없는 경우 알림
+//   // 디자인 추가?
+//   if(checkedDoc==null){
+//     alert("문서를 선택해주세요");
+//   }
 
-  // 팝업
-  // 모달 팝업으로 수정중
-  const url = `/approval/approvalDoc/${checkedDoc.value}`;
-  const name = "팝업"
-  const option = "width = 830, height = 940, top = 70, left = 530, location = no, resizable=no";
+//   // 팝업
+//   const url = `/approval/approvalDoc/${checkedDoc.value}`;
+//   const name = "팝업"
+//   const option = "width = 830, height = 940, top = 70, left = 530, location = no, resizable=no";
 
-  window.open(url, name, option);
-})
+//   window.open(url, name, option);
+// })
+
+
+// ===============================================================
+
+
+
+
