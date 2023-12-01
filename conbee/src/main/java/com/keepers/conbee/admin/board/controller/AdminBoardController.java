7841d560888f5de0922e3754d906007bc40d647d
kyuuuuu <mkmk0607@gmail.com>
2023-12-01 17:43:38 +0900

    231201 민규 - 유효성 검사

--- conbee/src/main/java/com/keepers/conbee/admin/board/controller/AdminBoardController.java
+++ conbee/src/main/java/com/keepers/conbee/admin/board/controller/AdminBoardController.java
@@ -1,5 +1,51 @@
 package com.keepers.conbee.admin.board.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin/boardManage")
 public class AdminBoardController {
 
+	
+	
+	/** 공지사항 관리 > 화면 전환
+	 * @return
+	 * @author 김민규
+	 */
+	@GetMapping("noticeBoard")
+	public String noticeBoard(){
+		return "admin/boardManage/noticeBoard";
+	}
+	
+	
+	/** 자유게시판 관리 > 화면 전환
+	 * @return
+	 * @author 김민규
+	 */
+	@GetMapping("freeBoard")
+	public String freeBoard(){
+		return "admin/boardManage/freeBoard";
+	}
+	
+	
+	/** 익명게시판 관리 > 화면 전환
+	 * @return
+	 * @author 김민규
+	 */
+	@GetMapping("anonymousBoard")
+	public String anonymousBoard(){
+		return "admin/boardManage/anonymousBoard";
+	}
+	
+
+	
+	
+
+	
+	
+	
+	
+	
 }
