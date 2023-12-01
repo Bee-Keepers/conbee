69e585f2e57dffc8c4061587247ec46699a876e9
lani <poikl11234@naver.com>
2023-12-01 17:45:52 +0900

    231201 yerina 점포리스트 페이지네이션, 수정페이지 작업중

--- conbee/src/main/java/com/keepers/conbee/admin/store/controller/AdminStoreControllder.java
+++ conbee/src/main/java/com/keepers/conbee/admin/store/controller/AdminStoreControllder.java
@@ -1,11 +1,16 @@
 package com.keepers.conbee.admin.store.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.keepers.conbee.admin.store.model.dto.Store;
 import com.keepers.conbee.admin.store.model.service.AdminStoreService;
@@ -25,16 +30,19 @@ public class AdminStoreControllder { // 관리자페이지 - 점포관리 컨트
 	 * @return storeList 전 점포 정보 리스트
 	 */
 	@GetMapping("storeList")
-	public String storeList(Model model) {
+	public String storeList(Model model,
+			@RequestParam(value = "cp", required = false, defaultValue = "1") int cp) {
 		
-		List<Store> storeList = service.readAllStoreList();
+		Map<String, Object> map = service.readAllStoreList(cp);
 		
-		model.addAttribute("storeList", storeList);
+		model.addAttribute("map", map);
 		
 		return "admin/storeManage/storeList";
 	}
 	
-	/** 점포정보수정 포워드
+	
+	
+	/** 점포정보수정 포워드 _ 점포명을 클릭해서 수정하는 경우가 아닐때
 	 * @author 예리나
 	 * @return
 	 */
@@ -42,6 +50,20 @@ public class AdminStoreControllder { // 관리자페이지 - 점포관리 컨트
 	public String storeUpdate() {
 		return "admin/storeManage/storeUpdate";
 	}
+	
+	
+	@GetMapping("storeUpdate/{storeNo:[0-9]+}")
+	public String storeUpdate(@PathVariable("storeNo") int storeNo,
+			Model model, RedirectAttributes ra) {
+		
+		
+		
+		return null;
+		
+	}
+	
+	
+	
 	
 	/** 신규점포등록 포워드
 	 * @author 예리나
--- conbee/src/main/java/com/keepers/conbee/admin/store/model/service/AdminStoreServiceImpl.java
+++ conbee/src/main/java/com/keepers/conbee/admin/store/model/service/AdminStoreServiceImpl.java
@@ -1,11 +1,15 @@
 package com.keepers.conbee.admin.store.model.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.stereotype.Service;
 
 import com.keepers.conbee.admin.store.model.dto.Store;
 import com.keepers.conbee.admin.store.model.mapper.AdminStoreMapper;
+import com.keepers.conbee.approval.model.dto.Pagination;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,12 +19,35 @@ public class AdminStoreServiceImpl implements AdminStoreService{
 
 	private final AdminStoreMapper mapper;
 	
+	
 	/** 점포정보조회 포워드
 	 *
 	 */
 	@Override
-	public List<Store> readAllStoreList() {
-		return mapper.readAllStoreList();
+	public Map<String, Object> readAllStoreList(int cp) {
+		
+		// 전체 점포수 얻어오기
+		int listCount = mapper.getListCount();
+		
+		/* cp, listCount를 이용해 Pagination 객체 생성*/
+		Pagination pagination = new Pagination(cp, listCount);
+		
+		// RowBounds 객체 생성
+		int offset = (pagination.getCurrentPage()-1) * pagination.getLimit();
+		
+		int limit = pagination.getLimit();
+		
+		RowBounds rowBounds = new RowBounds(offset, limit);
+		
+		// 마이바티스 호출
+		List<Map<String, Object>> storeList = mapper.readAllStoreList(rowBounds);
+		
+		// Map에 담아 반환
+		Map<String, Object> map = new HashMap<>();
+		map.put("pagination", pagination);
+		map.put("storeList", storeList);
+		
+		return map;
 	}
 	
 	
