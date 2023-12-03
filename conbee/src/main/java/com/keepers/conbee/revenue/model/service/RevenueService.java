37da3b5180e72d9126f190ad786bfd7d9a8ca63f
DESKTOP-9TSOIKI\윤성국 <dbs1128218@naver.com>
2023-12-04 02:04:05 +0900

    수습중

---
+++ conbee/src/main/java/com/keepers/conbee/revenue/model/service/RevenueService.java
@@ -0,0 +1,15 @@
+package com.keepers.conbee.revenue.model.service;
+
+import java.util.List;
+
+import com.keepers.conbee.revenue.model.dto.Revenue;
+
+public interface RevenueService {
+
+	/** 매출 상세 검색
+	 * @param revenue
+	 * @return
+	 */
+	List<Revenue> revenueSearch(Revenue revenue);
+
+}
