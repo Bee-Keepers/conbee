015aba55b3fc1bf08101cf0977c791aa1d8ab7c5
sanng97 <sanng97@naver.com>
2023-12-06 19:47:10 +0900

    마이페이지 수정중

--- conbee/src/main/java/com/keepers/conbee/mypage/model/mapper/MyPageMapper.java
+++ conbee/src/main/java/com/keepers/conbee/mypage/model/mapper/MyPageMapper.java
@@ -2,7 +2,7 @@ package com.keepers.conbee.myPage.model.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.keepers.conbee.member.model.dto.Member;
+
 
 @Mapper
 public interface MyPageMapper {
