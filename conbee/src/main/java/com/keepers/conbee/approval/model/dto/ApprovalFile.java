29eed87f69abec29ddc1edc50f814f29c55d5f5d
gamza2695 <gamza2695@gmail.com>
2023-12-11 18:33:46 +0900

    231211 yoojin-1 - 기안문 작성 파일첨부 구현 완료 - 기안문 작성 html, js 코드 정리 - 임시저장, 결재요청, 회수문서 작성중

--- conbee/src/main/java/com/keepers/conbee/approval/model/dto/ApprovalFile.java
+++ conbee/src/main/java/com/keepers/conbee/approval/model/dto/ApprovalFile.java
@@ -14,6 +14,8 @@ public class ApprovalFile {
 	private int approvalNo; // 문서번호
 	private int approvalFileNo; // 첨부파일 번호
 	private String approvalFileRoute; // 첨부파일 경로
+	private String approvalFileOriginName; // 원본 파일 이름
+	private String approvalFileRename; // 변경 파일 이름
 	
 	private MultipartFile uploadFile;
 
