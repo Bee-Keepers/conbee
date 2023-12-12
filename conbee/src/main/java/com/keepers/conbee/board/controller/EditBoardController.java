cd05cdebad26c60f3f1d9a0386079314e057eda0
kyuuuuu <mkmk0607@gmail.com>
2023-12-12 19:56:57 +0900

    231212 김민규 - 게시판 타이틀 작업 / 게시판 상세조회 디자인 등

--- conbee/src/main/java/com/keepers/conbee/board/controller/EditBoardController.java
+++ conbee/src/main/java/com/keepers/conbee/board/controller/EditBoardController.java
@@ -1,7 +1,12 @@
 package com.keepers.conbee.board.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
@@ -9,6 +14,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.keepers.conbee.board.model.dto.Board;
+import com.keepers.conbee.board.model.service.BoardService;
 import com.keepers.conbee.board.model.service.EditBoardService;
 import com.keepers.conbee.member.model.dto.Member;
 
@@ -23,6 +29,9 @@ import lombok.extern.slf4j.Slf4j;
 public class EditBoardController {
 	
 	private final EditBoardService service;
+	
+	private final BoardService boardService; // 게시글 수정 시 상세조회 호출용
+	
 
 	/*ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ*/
 	/*ㅡㅡㅡㅡㅡㅡㅡㅡㅡ 1. 게시글 작성 ㅡㅡㅡㅡㅡㅡㅡㅡㅡ*/	
@@ -44,9 +53,8 @@ public class EditBoardController {
 		RedirectAttributes ra
 		) {
 //		board.setMemberNo(loginMember.getMemberNo());
-//		board.setBoardCode(boardCode);
+//		board.setBoardCodeNo(boardCodeNo);
 		
-		log.info("==-=-=-=-=" + board);
 		
 		int boardNo = service.boardWrite(board);
 		
@@ -60,11 +68,40 @@ public class EditBoardController {
 	}
 	
 	
-	/*ㅡㅡㅡㅡㅡㅡㅡㅡㅡ상세 화면 만들기ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ*/
 	
 	
 	
 	
 	
 	/*ㅡㅡㅡㅡㅡㅡㅡㅡ수정 / 삭제ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ*/
+//	@GetMapping("boardUpdate/{boardCodeNo:[0-9]+}/{boardNo:[0-9]+}/update")
+//	public String boardUpdate(
+//		@PathVariable("boardCodeNo") int boardCodeNo,
+//		@PathVariable("boardNo") int boardNo,
+//		Model model) {
+//		
+//		Map<String, Object> map = new HashMap<>();
+//		map.put("boardCodeNo", boardCodeNo);
+//		map.put("boardNo", boardNo);
+//		
+//		Board board = boardService.boardDetail(map);
+//		
+//		model.addAttribute("board", board);
+//		
+//		
+//		return "board/boardUpdate";
+//	}
+//	
+	
+	@GetMapping("boardUpdate")
+	public String boardUpdate() {
+		
+		return "board/boardUpdate";
+	}
+	
+	
+	
+	
+	
+	
 }
--- conbee/src/main/java/com/keepers/conbee/board/model/mapper/BoardMapper.java
+++ conbee/src/main/java/com/keepers/conbee/board/model/mapper/BoardMapper.java
@@ -1,8 +1,10 @@
 package com.keepers.conbee.board.model.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.RowBounds;
 
 import com.keepers.conbee.board.model.dto.Board;
 
@@ -10,10 +12,11 @@ import com.keepers.conbee.board.model.dto.Board;
 public interface BoardMapper {
 
 	/** 게시글 목록 조회 
+	 * @param rowBounds 
 	 * @param boardCode
 	 * @return
 	 */
-	List<Board> selectBoardList(int boardCodeNo);
+	List<Board> selectBoardList(RowBounds rowBounds, int boardCodeNo);
 
 	/** 전체 게시글 수 조회
 	 * @param boardCode
@@ -21,4 +24,23 @@ public interface BoardMapper {
 	 */
 	int getListCount(int boardCodeNo);
 
+	/** 게시글 상세조회
+	 * @param map
+	 * @return
+	 */
+	Board boardDetail(Map<String, Object> map);
+
+	/** 게시판에(공지,자유 등) 이름 가져오기
+	 * @param boardCodeNo
+	 * @return
+	 */
+	String selectBoardName(int boardCodeNo);
+
+	/** 북마쿠 여부 확인
+	 * @param map
+	 * @return
+	 */
+	int bookMark(Map<String, Object> map);
+
+	
 }
