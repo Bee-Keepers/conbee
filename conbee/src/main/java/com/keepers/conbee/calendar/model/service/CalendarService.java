1770300fb151cdb7683f5d359ff797625712db9a
sanng97 <sanng97@naver.com>
2023-12-14 19:18:05 +0900

    캘린더 사원 점주 달력조회 일정추가 완료

--- conbee/src/main/java/com/keepers/conbee/calendar/model/service/CalendarService.java
+++ conbee/src/main/java/com/keepers/conbee/calendar/model/service/CalendarService.java
@@ -1,9 +1,25 @@
 package com.keepers.conbee.calendar.model.service;
 
+import java.util.List;
 
+import com.keepers.conbee.calendar.model.dto.Calendar;
+import com.keepers.conbee.member.model.dto.Member;
 
 public interface CalendarService {
+
+	/** 같은 팀의 일정 모두 조회
+	 * @param loginMember
+	 * @return calendarList
+	 */
+	List<Calendar> selectCalendar(Member loginMember);
 	
 	
+	/** 일정 추가
+	 * @param cal
+	 * @return result
+	 */
+	int staffcalendar(Calendar cal);
+
+
 
 }
