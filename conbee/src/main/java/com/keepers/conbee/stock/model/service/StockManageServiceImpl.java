bc00ec23bd2b8d08ff46acbe5492ad90d22bcd68
sungkuk <dbs1128218@naver.com>
2023-12-15 10:45:18 +0900

    재고, 상품 최종 수정 완료

--- conbee/src/main/java/com/keepers/conbee/stock/model/service/StockManageServiceImpl.java
+++ conbee/src/main/java/com/keepers/conbee/stock/model/service/StockManageServiceImpl.java
@@ -96,4 +96,15 @@ public class StockManageServiceImpl implements StockManageService{
 		}
 		return mapper.goodsSearch(stock);
 	}
+	
+	// 본사 재고 검색
+	@Override
+	public List<Stock> stockListSearch(Stock stock) {
+		if(stock.getLcategoryName() == null && stock.getScategoryName() == null && stock.getGoodsName() == null) {
+			stock.setLcategoryName("");
+			stock.setScategoryName("");
+			stock.setGoodsName("");
+		}
+		return mapper.stockListSearch(stock);
+	}
 }
--- conbee/src/main/resources/static/js/stock/stockManageList.js
+++ conbee/src/main/resources/static/js/stock/stockManageList.js
@@ -1,31 +1,3 @@
-const storeNoSelect = document.getElementById("storeNoSelect");
-const storeSearch = document.getElementById("storeSearch");
-// 지점 이름으로 검색
-storeSearch.addEventListener("change", e=>{
-
-const storeSearch = document.getElementById("storeSearch");
-const storeSelect = document.getElementById("storeSelect");
-// 지점 이름으로 검색
-storeSearch.addEventListener("change", e=>{
-    fetch("/revenueManage/storeSearch?inputStoreName=" + e.target.value)
-    .then(resp=>resp.json())
-    .then(list=>{
-        storeSelect.innerHTML = "";
-        console.log(list);
-        if(list.length == 0){
-            storeSelect.innerText = "검색된 지점이 없습니다.";
-        } else{
-            for(let opt of list){
-                const option = document.createElement("option");
-                option.value = opt.storeNo;
-                option.innerText = opt.storeName;
-                storeSelect.append(option);
-            }
-        }
-    })
-    .catch(e=>console.log(e));
-});
-
 const url = new URL(location.href);
 const urlParams = url.searchParams;
 const options = document.querySelectorAll("#storeNoSelect>option");
@@ -186,7 +158,7 @@ stockUpdateBtn.addEventListener("click", () => {
   document.getElementById("goodsName").value = row.children[2].innerText;
   document.getElementById("lcategoryNameUpdate").value = row.children[3].innerText;
   document.getElementById("scategoryNameUpdate").value = row.children[4].innerText;
-  document.getElementById("stockInPrice").value = row.children[6].innerText;
+  document.getElementById("stockInPriceUpdate").value = row.children[6].innerText;
   document.getElementById("stockOutPriceUpdate").value = row.children[7].innerText;
   document.getElementById("stockDiscountUpdate").value = row.children[9].innerText;
   document.getElementById("storeNoUpdate").value = row.children[11].innerText;
@@ -229,4 +201,10 @@ const stockInertReset = document.getElementById("stockInertReset");
 /* 등록 초기화 버튼 클릭 시 데이터 초기화 */
 stockInertReset.addEventListener("click", () => {
   document.getElementById("stockInertForm").reset();
+});
+
+const stockSearchReset = document.getElementById("stockSearchReset");
+/* 등록 초기화 버튼 클릭 시 데이터 초기화 */
+stockSearchReset.addEventListener("click", ()=>{
+  document.getElementById("stockSearchForm").reset();
 });
