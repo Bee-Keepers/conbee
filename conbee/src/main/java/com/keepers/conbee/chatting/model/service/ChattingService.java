2769d580e71d8d60fbfc16b55beb76920e3a8bf7
kyuuuuu <mkmk0607@gmail.com>
2023-12-19 14:00:40 +0900

    231219 김민규 - 채팅 sql 수정 / 채팅 구문 작성 등

---
+++ conbee/src/main/java/com/keepers/conbee/chatting/model/service/ChattingService.java
@@ -0,0 +1,27 @@
+package com.keepers.conbee.chatting.model.service;
+
+import java.util.List;
+
+import com.keepers.conbee.chatting.model.dto.Chatting;
+
+public interface ChattingService {
+
+	/** 1:1 채팅 조회
+	 * @param memberNo
+	 * @return
+	 */
+//	List<Chatting> selectChatList1(int memberNo);
+
+	/** 팀 채팅 조회
+	 * @param teamNo
+	 * @return
+	 */
+//	List<Chatting> selectChatList2(int teamNo);
+
+	/** 채팅 조회 (테스트)
+	 * @param memberNo
+	 * @return
+	 */
+	List<Chatting> selectChat(int memberNo);
+
+}
